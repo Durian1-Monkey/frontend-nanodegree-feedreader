1d2c6f27972207f3969bb6ca61f613742cc613f3
Durian Monster <morita657@gmail.com>
2016-03-20 22:49:20 +0700

    Add test menu icon to show and hidden

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,7 @@ $(function() {
          * and that the URL is not empty.
          */
 //         console.log(allFeeds[1].url);
+/*
          var feedLen = allFeeds.length;
          var box = [];
          for(var i =0; i < feedLen; i++) {
@@ -42,7 +43,7 @@ $(function() {
 //            expect(allFeeds[i].name).toBeDefined();
 //            expect(allFeeds.url).not.toBeDefined();
             });
-         }
+         }*/
 
             it('URLs are not empty', function() {
                 allFeeds.forEach(function(allFeeds) {
@@ -65,18 +66,32 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is defined', function() {
+            expect(document.body.classList).toContain('menu-hidden');
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+//display the menu
+        it('is showen', function() {
+            $('#menu-icon-link').trigger('click');
+            expect(document.body.classList).not.toBe('menu-hidden');
+        });
+//hidden the menu
+        it('is showen', function() {
+            $('#menu-icon-link').trigger('click');
+            expect(document.body.classList).toBe('menu-hidden');
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
